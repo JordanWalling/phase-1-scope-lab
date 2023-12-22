89cdeb738d9ebc5d45e30a7be63ec684682131ff
JordanWalling <jordanwalling1991@gmail.com>
2023-12-22 16:32:16 +1100

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+
+// // Instruction (1)
+var customerName = "bob";
+
+// Instruction (2)
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Instruction (3)
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// Instruction (4)
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// Instruction (5)
+const leastFavoriteCustomer = "dave";
+
+// Instruction (6)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "karen";
+}
